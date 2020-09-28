7e542ac184751088d3e7f915888563a0c9be3a13
Carl Brand <carl.brand@bit-g.co.uk>
2020-09-28 18:34:08 +0000

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+const commonjs = require( "@rollup/plugin-commonjs" );
+const resolve = require( "@rollup/plugin-node-resolve" );
+
+module.exports =
+{
+    input: "src/Scripts/main.js",
+
+    plugins: [
+        commonjs(),
+        resolve,
+    ],
+
+    output: {
+        file: "nova-elixirls.novaextension/Scripts/main.dist.js",
+        format: "cjs"
+    },
+}
